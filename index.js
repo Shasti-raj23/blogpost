c871b8ec6901983d8fb781c84b0a737e6e11bc94
Shasti-raj23 <shastiraj35@gmail.com>
2024-06-16 18:00:00 +0530

    routes such as post, patch , delete are add here

--- index.js
+++ index.js
@@ -53,10 +53,38 @@ app.get("/posts/:id",(req,res)=>{
 
 })
 //CHALLENGE 3: POST a new post
+app.post("/posts",(req,res)=>{
+  const newId = lastId+1
+  const post={
+    id: newId,
+    title : req.body.title,
+    content : req.body.content,
+    author : req.body.author,
+    date : new Date()
+  }
+  posts.push(post)
+  res.status(201).send(post)
+
+})
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
-
+app.patch("/posts/:id",(req,res)=>{
+  const post = posts.find((p)=>p.id === parseInt(req.params.id))
+  if(!post) return res.status(404).json({message:"post not found"});
+  
+  if(req.body.author) post.author = req.body.author;
+  if(req.body.title) post.title = req.body.title;
+  if(req.body.content) post.content = req.body.content;
+  res.json(post);
+})
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id",(req,res)=>{
+  const post = posts.find((p)=> p.id === parseInt(req.params.id))
+  if(!post) return res.status(404).send({message: "the post is not found"});
+
+  posts.splice(post.id,1)
+  res.send({message:"post deleted"});
+})
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
